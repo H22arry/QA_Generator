32cc82764947de1770807c52716647b7ac3a18a5
abhishek mutkekar <84004361+H22arry@users.noreply.github.com>
2024-10-07 16:02:05 +0530

    Update streamlit_app.py
    Form Creation

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ st.title('🎈 QA Generator')
 
 st.info('**What to Generate Some Question ???**')
 
-st.write('**Enter Some Paragraph ???**')
-user_input = st.text_area("Enter your text here", height=150)
-test_type = st.selectbox('Question Type',('Subjective','Objective'))
-no_question = st.slider('No of Question',1,5,10)
+st.write('**Enter Some Paragraph !!!**')
+with st.form("my_form"):
+  user_input = st.text_area("Enter your text here", height=150)
+  test_type = st.selectbox('Question Type',('Subjective','Objective'))
+  no_question = st.slider('No of Question',1,5,10)
+  st.form_submit_button('Submit my picks')
