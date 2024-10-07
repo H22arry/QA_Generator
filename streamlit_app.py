48274c6c0f45751c93a7359129026089894c3da3
abhishek mutkekar <84004361+H22arry@users.noreply.github.com>
2024-10-07 14:33:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,3 @@ st.info('**What to Generate Some Question ???**')
 
 user_input = st.text_area("Enter your text here", height=150)
 
-# Display the entered text
-if user_input:
-    st.write("You entered:")
-    st.write(user_input)
