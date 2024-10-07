fada3ef379fc8681e5e42402238e6bce80a4342f
abhishek mutkekar <84004361+H22arry@users.noreply.github.com>
2024-10-07 14:32:28 +0530

    Update streamlit_app.py
    text area

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.title('🎈 QA Generator')
 
 st.info('**What to Generate Some Question ???**')
+
+user_input = st.text_area("Enter your text here", height=150)
+
+# Display the entered text
+if user_input:
+    st.write("You entered:")
+    st.write(user_input)
