21930651d275166d8a66a031f5a1921411f5c0fe
abhishek mutkekar <84004361+H22arry@users.noreply.github.com>
2024-10-07 16:04:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
 st.title('🎈 QA Generator')
-
 st.info('**What to Generate Some Question ???**')
-
 st.write('**Enter Some Paragraph !!!**')
+
 with st.form("my_form"):
   user_input = st.text_area("Enter your text here", height=150)
   test_type = st.selectbox('Question Type',('Subjective','Objective'))
