c5485ee49f13a42435368217125e94066194193b
abhishek mutkekar <84004361+H22arry@users.noreply.github.com>
2024-10-08 12:47:39 +0530

    Update streamlit_app.py
    Writing the logic of subjective and objective

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,31 @@
 import streamlit as st
+from objective import ObjectiveTest
+from subjective import SubjectiveTest
 
 st.title('🎈 QA Generator')
-st.info('**What to Generate Some Question ???**')
+st.write('**Want to Generate Some Question ???**')
 st.write('**Enter Some Paragraph !!!**')
 
 with st.form("my_form"):
   user_input = st.text_area("Enter your text here", height=150)
   test_type = st.selectbox('Question Type',('Subjective','Objective'))
   no_question = st.slider('No of Question',1,5,10)
-  st.form_submit_button('Submit my picks')
-st.write(user_input)
+  
+  submit_button = st.form_submit_button('Submit my picks')
+
+if submit_button:
+  if no_question<=0:
+    st.error("Number of question should be greater than zero")
+  else:
+    if test_type == "Objective":
+      objective_generator = ObjectiveTest(user_input, no_question)
+      question_list, answer_list = objective_generator.generate_test()
+    elif testType == "subjective":
+      subjective_generator = SubjectiveTest(user_input, no_question)
+      question_list, answer_list = subjective_generator.generate_test()
+    
+    # Display the generated questions and answers
+    st.write("### Generated Test")
+    for i, (question, answer) in enumerate(zip(question_list, answer_list), start=1):
+        st.write(f"**Q{i}:** {question}")
+        st.write(f"**A{i}:** {answer}")
